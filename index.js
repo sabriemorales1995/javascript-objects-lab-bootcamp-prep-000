d588117e4977d800b423f180675993e3dadc1111
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-02 02:19:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
- var temp = Obj.assign({}, object)
+ var temp = Object.assign({}, object)
  delete temp[key];
  return temp;
 }
