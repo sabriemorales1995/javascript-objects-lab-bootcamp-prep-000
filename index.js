5e90e47364bcebb66e6d7e5f0ecd4719444b1164
Sabrie Morales <sabriemorales1995@gmail.com>
2018-07-02 02:06:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var recipes = {};
 
-updateObjectsWithKeyAndValue(object , key, value){
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({}, object, {[key]:value});
   //return an object with the original key value pairs and the new key value pair
 }
 
